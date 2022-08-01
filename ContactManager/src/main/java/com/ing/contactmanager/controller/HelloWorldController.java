87a8d977c4fd2793fca6a75cad569f3deaea9cc1
stefan-gg <stefan.gogic01@gmail.com>
2022-08-01 12:31:51 +0200

    Initial commit, project structure added

---
+++ ContactManager/src/main/java/com/ing/contactmanager/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.ing.contactmanager.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello_world")
+public class HelloWorldController {
+
+    @GetMapping
+    public ResponseEntity<String> getHelloWorld(){
+        return new ResponseEntity<String>("Hello World", HttpStatus.OK);
+    }
+}
