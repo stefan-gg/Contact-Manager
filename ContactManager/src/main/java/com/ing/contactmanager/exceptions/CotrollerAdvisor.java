d60fe091d58bea1c0b4e94803f830c8f97be7cc5
stefan-gg <stefan.gogic01@gmail.com>
2022-08-07 15:14:38 +0200

    @ControllerAdvice annotation added, ControllerAdvisor class added

---
+++ ContactManager/src/main/java/com/ing/contactmanager/exceptions/CotrollerAdvisor.java
@@ -0,0 +1,27 @@
+package com.ing.contactmanager.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class CotrollerAdvisor extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<Object> handleNoSuchEntityException(NoSuchElementException ex, WebRequest request){
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", "Element not found");
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
