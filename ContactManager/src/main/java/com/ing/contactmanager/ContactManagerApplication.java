cc216c0fd91bb3b20b4482630e265587dabbc360
stefan-gg <stefan.gogic01@gmail.com>
2022-08-19 10:41:54 +0200

    Docker files added

--- ContactManager/src/main/java/com/ing/contactmanager/ContactManagerApplication.java
+++ ContactManager/src/main/java/com/ing/contactmanager/ContactManagerApplication.java
@@ -2,6 +2,7 @@ package com.ing.contactmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class ContactManagerApplication {
